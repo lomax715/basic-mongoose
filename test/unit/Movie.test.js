bd8e285ae012e495a486602b8334d45c194eddf4
Jack Lomax <lomax15@me.com>
2018-04-18 11:37:42 -0700

    functional test for title of movie required

--- test/unit/Movie.test.js
+++ test/unit/Movie.test.js
@@ -21,4 +21,17 @@ describe('Movie', () => {
 
         assert.isUndefined(movie.validateSync());
     });
+
+    const getValidationErrors = validation => {
+        assert.isDefined(validation, 'no validation errors');
+        return validation.errors;
+    };
+
+    it('name is required field', () => {
+        const movie = new Movie({});
+        const errors = getValidationErrors(movie.validateSync());
+        assert.equal(Object.keys(errors).length, 1);
+        assert.equal(errors.title.kind, 'required');
+    });
+
 });
